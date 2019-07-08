17b49ce9b2fc4e36c45df783dccab56eff01da34
Shelby Miller <shelbycrosbee@gmail.com>
2019-07-08 12:09:00 -0600

    make links route

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,14 +4,17 @@ import { Col, Row } from 'react-bootstrap'
 import Contact from './Contact/Contact'
 import ResumeLink from './Resume/ResumeLink'
 import PortfolioLink from "./Portfolio/PortfolioLink";
+import { Route, Switch, Link, withRouter } from 'react-router-dom';
 
 
 class Layout extends React.Component {
 
+ 
 
   render() {
 
     return (
+      
 <Row>
   <Col><Contact/></Col>
   <Col><ResumeLink /></Col>
--- src/components/Portfolio/PortfolioLink.js
+++ src/components/Portfolio/PortfolioLink.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {Button} from 'react-bootstrap'
+import { withRouter } from 'react-router-dom'
 
 class PortfolioLink extends React.Component {
 
-
+handlePortfolioLink(e){
+  e.preventDefault();
+  this.props.history.push('/portfolio')
+}
 
   render() {
     return (
-      <Button variant={'secondary'}>Portfolio</Button>
+      <Button onClick = {e => this.handlePortfolioLink(e)} variant={'dark'}>Portfolio</Button>
     )
   }
 }
 
-export default PortfolioLink
+export default withRouter(PortfolioLink)
