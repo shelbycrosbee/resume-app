7b29e205dee2eda3b98dde3170664cce5cb21310
Shelby Miller <shelbycrosbee@gmail.com>
2019-07-08 11:30:04 -0600

    ooof

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import './App.css';
-import NavbarComponent from "./components/Navbar";
 import BackgroundImagePage from "./components/BackgroundImagePage";
-import Contact from "./components/Contact/Contact";
 import Resume from "./components/Resume/Resume";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row"
-import Button from "react-bootstrap/Button"
-import { Route, Switch, Link, withRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout'
 import Portfolio from './components/Portfolio/Portfolio'
 
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,10 +4,23 @@ import { Col, Row } from 'react-bootstrap'
 import Contact from './Contact/Contact'
 import ResumeLink from './Resume/ResumeLink'
 import PortfolioLink from "./Portfolio/PortfolioLink";
+import river from '../images/owyhee.jpg'
+import ski from '../images/ski.jpg'
 
 
+const pictures = {
+  river,
+  ski
+}
 class Layout extends React.Component {
-
+  constructor(props){
+    super(props)
+  
+  this.state = {
+    pictures: null
+  }
+  }
+  
 
   render() {
 
