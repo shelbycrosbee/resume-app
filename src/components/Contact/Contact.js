5528a486861108ee3a2d0eef99dbaddd4f9dcfef
Shelby Miller <shelbycrosbee@gmail.com>
2019-08-16 12:52:19 -0600

    contact font better

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -16,12 +16,12 @@ render(){
     <u>Contact</u>
      </Dropdown.Toggle>
       <Dropdown.Menu>
-      <Dropdown.Item href="tel:+15413069775" target="_blank">📞 (541)306-9775</Dropdown.Item>
-        <Dropdown.Item href="mailto:shelbycrosbee@gmail.com" target="_blank"><i  className="far fa-envelope" ></i> shelbycrosbee@gmail.com
+      <Dropdown.Item className='navbarFont' href="tel:+15413069775" target="_blank">📞 (541)306-9775</Dropdown.Item>
+        <Dropdown.Item className='navbarFont' href="mailto:shelbycrosbee@gmail.com" target="_blank"><i  className="far fa-envelope" ></i> shelbycrosbee@gmail.com
         </Dropdown.Item>
-        <Dropdown.Item  href="https://github.com/shelbycrosbee"><i className="fab fa-github-square"></i> shelbycrosbee
+        <Dropdown.Item className='navbarFont' href="https://github.com/shelbycrosbee"><i className="fab fa-github-square"></i> shelbycrosbee
         </Dropdown.Item>
-        <Dropdown.Item href="https://www.linkedin.com/in/shelby-miller-00754a182/"
+        <Dropdown.Item className='navbarFont' href="https://www.linkedin.com/in/shelby-miller-00754a182/"
             target="_blank"><i  className="fab fa-linkedin" ></i> Shelby
             Miller</Dropdown.Item>
             </Dropdown.Menu>
