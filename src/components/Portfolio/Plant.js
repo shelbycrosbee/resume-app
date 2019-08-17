c917ee887e359113e51752c18881cc1a793b1b7f
Shelby Miller <shelbycrosbee@gmail.com>
2019-08-16 18:50:58 -0600

    alright stopping now

--- src/components/Portfolio/Plant.js
+++ src/components/Portfolio/Plant.js
@@ -3,20 +3,21 @@ import { Carousel, Image } from 'react-bootstrap';
 import rPlant from '../../images/plant.jpg'
 import '../../index.css'
 import deer from '../../images/deer.JPG'
+import terry from '../../images/terry.jpeg'
 
 class Plant extends React.Component {
 
   render() {
     return (
 
-      <Carousel className='mx-auto' style={{height: '700px'}}>
+      <Carousel className='mx-auto' style={{height: '800px', innerWidth: '100px'}}>
         <Carousel.Item>
           <Image
-            className="d-block w-100 carousel"
+            className="d-block  mx-auto carousel"
             src={rPlant}
             alt="robert plant"
-            fluid
           />
+          <br/>
           <Carousel.Caption>
             <h3>Drawing of Robert Plant</h3>
             <p>Charcoal 6ft by 4ft</p>
@@ -24,7 +25,7 @@ class Plant extends React.Component {
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100 carousel"
+            className="d-block mx-auto carousel"
             src={deer}
             alt="deer"
           />
@@ -35,13 +36,13 @@ class Plant extends React.Component {
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100 carousel"
-            src={deer}
-            alt="deer"
+            className="d-block mx-auto terry"
+            src={terry}
+            alt="terry"
           />
           <Carousel.Caption>
-            <h3>Painting of a Buck</h3>
-            <p>Watercolor 4ft by 2.5ft</p>
+            <h3>Abstract</h3>
+            <p>Oil 3ft by 2ft</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
