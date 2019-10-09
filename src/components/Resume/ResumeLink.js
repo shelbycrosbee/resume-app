2107a047c84964de13ea56534dbce61a3e9c04c7
Shelby Miller <shelbycrosbee@gmail.com>
2019-10-09 14:30:45 -0600

    push

--- src/components/Resume/ResumeLink.js
+++ src/components/Resume/ResumeLink.js
@@ -14,7 +14,7 @@ class ResumeLink extends React.Component {
   render() {
     return (
  
-      <a onClick={e => this.handleResumeLink(e)} className='resHeader' style={{color: "white", hover: "black"}}>RESUME</a>
+      <a onClick={e => this.handleResumeLink(e)} className='resHeader' style={{color: "white"}}>RESUME</a>
       
     )
   }
